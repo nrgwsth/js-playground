a3aa02ac66389742581ca15fc0b7436f1232fc76
anuragasaurus <anuragawasthi13@gmail.com>
2018-03-16 23:00:14 +0530

    changed styles, refctored code

--- src/App.js
+++ src/App.js
@@ -1,224 +1,146 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-import {StyleSheet, css} from "aphrodite/no-important";
 
-const styles = StyleSheet.create({
-  "editor":{
-    width: "calc(100% - 10px)",
-    margin: "5px",
-    minHeight: "250px",
-    resize: "none",
-    overflowY: "scroll",
-    fontSize: "20px",
-    border: "1px solid #555",
-    ":focus":{
-      outline: "none"
-    }
-  },
-
-  result:{
-    width: "calc(100% - 30px)",
-    margin: "5px",
-    padding: "10px",
-    minHeight: "250px",
-    fontSize: "14px",
-    fontFamily: "'PT Sans', sans-serif",
-    border: "1px solid #444"
-  },
-
-  btn:{
-    padding: "5px 10px",
-    margin: "2px",
-    border: "none",
-    backgroundColor: "#888",
-    cursor: "pointer",
-    transition: "background-color 0.5s",
-    ":hover":{
-      backgroundColor: "#bbb"
-    },
-    ":focus":{
-      outline: "none"
-    }
-  },
-
-
-  buttonWrapper:{
-    float: "right"
-  },
-
-  "smallmargin": {
-    margin: "5px",
-    fontFamily: "'Merriweather', serif"
-  }
-})
-
-const COLOR = {
-  NORMAL: "#AED6F1",
-  EDITOR: "#F4F6F7"
-}
+let codeSamples = require("./exercises.json")
 
 class App extends Component {
-
   constructor(props){
     super(props);
-    this.currentCodeSample = this.currentCodeSample.bind(this);
-    this.onDoubleClick = this.onDoubleClick.bind(this);
-    this.prevClick = this.prevClick.bind(this);
-    this.nextClick = this.nextClick.bind(this);
-    this.runClick = this.runClick.bind(this);
-    this.onChange = this.onChange.bind(this);
+    this.onPrevClick = this.onPrevClick.bind(this);
+    this.onNextClick = this.onNextClick.bind(this);
 
     this.state = {
-      codeSamples: [],
       index: 0,
       inputValue: "",
       editing: false,
-      styles:{
-        textareaBackgroundColor: COLOR.NORMAL
-      }
     }
 
   }
 
   componentWillMount(){
-    const codeSamples = [{
-      codeString: "var a = 5;var b = 5;assert(a!==undefined, 'a is not undefined')",
-      heading: "variable declaration",
-      subHeading: "simple heading",
-      explanation: "simple explanation"
-    },{
-      codeString: "function func1(){};var func2 = function(){};window.func3 = function(){};assert(func1!==undefined, 'func1 exists');assert(func2!==undefined, 'func2 exists');assert(func3!==undefined, 'func3 exists')",
-      heading: "function declaration",
-      subHeading: "simple heading",
-      explanation: "simple explanation"
-    },{
-      codeString: "var x = 'outer';function test(inner) {if (inner) {var x = 'inner';return x;}return x;}assert(test(false) === undefined, 'it should return undefined as var declaration are hoisted');assert(test(true) === 'inner', 'will get redefined.')",
-      heading: "Hoisting of variable declaration with var",
-      subHeading: "simple heading",
-      explanation: "declaration with var are hoisted, so inner x will move to top of the function body."
-    }];
-
     this.setState({
       codeSamples: codeSamples
     });
 
     window.hljs.configure({useBR: true});
-    window.assert = function(condition, message){
-
-      var result = document.getElementById("result");
-      var type = condition ? "PASS" : "FAIL";
-
-      var htmlRaw = "<div style='margin: 10px 0;font-size: 18px; font-weight: bold;'><span style='color: red; font-size: 20px; padding: 0 10px;'>"+type+"</span><span>"+message+"</span></div>";
-
-      result.insertAdjacentHTML("beforeend", htmlRaw);
-    }
   }
 
-  currentCodeSample(){
-    const {state} = this;
-    console.log(state);
-
-    return state.editing ? state.inputValue : window.js_beautify(state.codeSamples[state.index].codeString);
-
-  }
-
-  prevClick(){
+  onPrevClick(){
     this.setState({
       editing: false,
-      index: this.state.index - 1 >=0 ? this.state.index - 1 : 0,
-      styles:{
-        ...this.state.styles,
-        textareaBackgroundColor: COLOR.NORMAL
-      }
+      index: this.state.index - 1 >=0 ? this.state.index - 1 : 0
     })
-
-    document.getElementById("result").innerHTML = "";
-    console.log("result", document.getElementById("result"))
   }
 
-  nextClick(){
+  onNextClick(){
     this.setState({
       editing: false,
-      index: this.state.index >= this.state.codeSamples.length-1? this.state.codeSamples.length-1 : this.state.index + 1,
-      styles:{
-        ...this.state.styles,
-        textareaBackgroundColor: COLOR.NORMAL
-      }
-    })
-
-    document.getElementById("result").innerHTML = "";
-
-  }
-
-  runClick(){
-    document.getElementById("result").innerHTML = "";
-    eval(this.currentCodeSample());
-  }
-
-  onDoubleClick(){
-    this.setState({
-      editing: true,
-      inputValue: window.js_beautify(this.state.codeSamples[this.state.index].codeString),
-      styles:{
-        ...this.state.styles,
-        textareaBackgroundColor: COLOR.EDITOR
-      }
-    })
-  }
-
-
-  onChange(e){
-    this.setState({
-      inputValue: e.target.value
+      index: this.state.index >= this.state.codeSamples.length-1? this.state.codeSamples.length-1 : this.state.index + 1
     })
   }
 
   render() {
     return (
-      <div className="wrapper">
-        <div>
-          <h2 className={css(styles.smallmargin)}>{this.state.codeSamples[this.state.index].heading}</h2>
-          <small className={css(styles.smallmargin)}>{this.state.codeSamples[this.state.index].subHeading}</small>
-          <div className={css(styles.buttonWrapper)}>
-            <button onClick={this.prevClick} className={css(styles.btn)}>Prev</button>
-            <button onClick={this.nextClick} className={css(styles.btn)}>Next</button>
-            <button onClick={this.runClick} className={css(styles.btn)}>Run</button>
-          </div>
-          {this.state.editing &&
-            <textarea
-            className={css(styles.editor)}
-            style={{
-              backgroundColor: this.state.styles.textareaBackgroundColor
-            }}
-            value = {this.currentCodeSample()}
-            onDoubleClick = {this.onDoubleClick}
-            onChange = {this.onChange}
-          >
-          </textarea>}
-
-          {!this.state.editing &&
-            <pre
-              className={css(styles.editor)}
-              style={{
-                backgroundColor: this.state.styles.textareaBackgroundColor
-              }}
-              onDoubleClick = {this.onDoubleClick}
-              onChange = {this.onChange}
-              dangerouslySetInnerHTML = {{__html: window.hljs.highlight("javascript", this.currentCodeSample()).value}}
-            >
-          </pre>}
-        </div>
-
-        <div className={css(styles.result)}>
-          <div>
-            {this.state.codeSamples[this.state.index].explanation}
-          </div>
-          <div id="result">
-          </div>
+      <div className = "rootdiv">
+        <Header onNextClick = {this.onNextClick} onPrevClick = {this.onPrevClick} />
+        <div className = "editorWraper">
+           <LeftEditor data = {codeSamples[this.state.index].content} />
+           <RightEditor data = {codeSamples[this.state.index].codeString} />
         </div>
       </div>
     );
+  }
+}
+
+
+class Header extends Component{
+  render(){
+    return(
+      <div className = "headerWrapper">
+        <div className = "leftHeader">
+           JS-PLAYGROUND
+        </div>
+        <div className = "rightHeader">
+          <button onClick = {this.props.onNextClick}>Next</button>
+          <button onClick = {this.props.onPrevClick}>Prev</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+class LeftEditor extends Component{
+  render(){
+    const {data} = this.props
+    return(
+      <div className = "leftEditor">
+        <div className = "leftEditorHeader">{data.heading}</div>
+        <div className = "leftEditorBody" dangerouslySetInnerHTML={{__html: marked(data.contentMD)}}></div>
+      </div>
+    )
+  }
+}
+
+class RightEditor extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      codeString: window.js_beautify(props.data),
+      editing: false
+    }
+  }
+
+  runClick(){
+    const assert = `function assert(condition, msg){
+      const html = document.getElementById("result").innerHTML
+      const classprefix = condition ? "pass" : "fail"
+      document.getElementById("result").innerHTML = html + "<div><span class='result-span-"+classprefix+"'>"+classprefix+"</span><span class='result-span-message'>"+msg+"</span></div>"
+    }`
+    var func = new Function(assert + this.state.codeString)
+    try{
+      func()
+    } catch(e){
+      console.log(error)
+    }
+  }
+
+  onChange(e){
+    this.setState({
+      codeString: e.target.value
+    })
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(this.props.data !== nextProps.data){
+      this.setState({
+        codeString: window.js_beautify(nextProps.data),
+        editing: false
+      })
+      document.getElementById("result").innerHTML = ""
+    }
+  }
+
+  render(){
+    return(
+      <div className = "rightEditor">
+        {!this.state.editing ?
+          <div
+            className = "rightEditorBody"
+            onDoubleClick = {()=>this.setState({editing: true})}
+            dangerouslySetInnerHTML = {{__html: window.hljs.highlight("javascript", this.state.codeString).value}}
+          ></div>
+          :
+          <textarea
+            className = "rightEditorBody"
+            value = {this.state.codeString}
+            onChange = {this.onChange.bind(this)}
+          />
+        }
+        <div className = "floatingRunButton">
+          <button onClick = {this.runClick.bind(this)}>Run</button>
+        </div>
+        <div id = "result"></div>
+      </div>
+    )
   }
 }
 
